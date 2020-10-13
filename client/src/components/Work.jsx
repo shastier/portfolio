ee72eea3df1a7cee5362d1020ee9103370196700
Shirley Hastier <shastier@yahoo.com>
2020-10-13 13:14:07 -0500

    FrondEnd, projects, styling

--- client/src/components/Work.jsx
+++ client/src/components/Work.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Work = (props) => {
     return (
         <div className="project" key={props.project.id}>
-            <img className="project-img" src={props.project.id.img_url} alt="project goes here" />
+            <img className="project-img" sizes="250px"  src={props.project.id.img_url} alt="project goes here" />
             <div className="project-content">
                 <span>{props.project.id.name}</span>
             </div>
